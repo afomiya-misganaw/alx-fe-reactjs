7658934e70068cfc486687d461de5ade97ba6410
afomiya-misganaw <afomiyamisganaw55@gmail.com>
2025-08-22 17:15:02 +0300

    Create App.jsx

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import BlogPost from './components/BlogPost';
+import ProtectedRoute from './components/ProtectedRoute';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/blog/:postId" element={<BlogPost />} />
+          <Route path="/profile/*" element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
